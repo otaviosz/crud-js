01ac79a7c8b090b35b2f09051e5358b01715c1bd
Otávio <otaviodiascc@gmail.com>
2024-07-05 11:42:52 -0300

    crud - create

--- main.js
+++ main.js
@@ -6,6 +6,27 @@ const openModal = () => document.getElementById('modal')
 const closeModal = () => document.getElementById('modal')
     .classList.remove('active')
 
+
+const tempClient = {
+    nome: "frank",
+    email: "frankdias@gmail.com",
+    celular: "16991558344",
+    cidade: "Passos"
+}
+
+const getLocalStorage = () => JSON.parse(localStorage.getItem('db_client')) ?? [];
+const setLocalStorage = (dbClient) => localStorage.setItem("db_client", JSON.stringify(dbClient));
+
+// Crud - Cread Read Update Delete
+
+
+const createClient = (client) => {
+    const dbClient = getLocalStorage();
+    dbClient.push(client)
+    setLocalStorage(dbClient)
+}
+
+// Eventos
 document.getElementById('cadastrarCliente')
     .addEventListener('click', openModal)
 
